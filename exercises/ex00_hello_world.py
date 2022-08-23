550567b0d4dd7ada44b37c69372f854dfb7e3c68
Joshua Vazquez <vazquezj@unc.edu>
2022-08-22 23:24:44 -0400

    Finished Exercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,5 @@
+"""My first program for COMP110. EX00 Hello, world."""
+
+__author__ = 730622831
+
+print("Hello, world. My name is Joshua James Vazquez y en solo seis meses me voy a convertir en el mejor programador que pueda.")
